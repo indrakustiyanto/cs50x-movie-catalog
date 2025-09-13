b12ec405a2e8e85cc3bb6868e7b4b8ee036b4c5e
indra saputro <indrasaputro65@gmail.com>
2025-09-13 17:28:32 +0700

    feat: adding basic search icon functionality

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,36 +1,4 @@
-const show = document.querySelector('.show');
-
-
-// func to fetch movies from backend
-// async function searchMovies() {
-//   const query = document.getElementById('search').value;
-//   const response = await axios.get(`http://127.0.0.1:5000/search?q=${query}`)
-//   const movies = response.data.results;
-//   console.log(movies);
-
-//   const results = document.getElementById('results');
-//   results.innerHTML = '';
-
-
-//   // Display movies
-//   movies.forEach(movie => {
-//     const movieCard = `
-//       <div class="bg-gray-800 rounded-2xl shadow-lg overflow-hidden hover:scale-105 transition-transform">
-//         <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}" class="w-full h-96 object-cover">
-//         <div class="p-4">
-//           <h3 class="text-xl font-bold mb-2">${movie.title} 
-//             <span class="text-gray-400 text-sm">(${movie.release_date?.split("-")[0] || "N/A"})</span>
-//           </h3>
-//           <p class="text-sm text-gray-300 mb-2"><b>⭐ ${movie.vote_average}</b> (${movie.vote_count} votes)</p>
-//           <p class="text-gray-400 text-sm">${movie.overview ? movie.overview.substring(0, 100) + "..." : "No description available."}</p>
-//         </div>
-//       </div>
-//     `;
-//     results.innerHTML += movieCard;
-//     })
-// };
-
-// document.getElementById('searchBtn').addEventListener('click', searchMovies);
+// function to fetch trending movies and display the backdrop of the first movie and poster
 
 async function fetchTrending() {
   const response = await axios.get('http://127.0.0.1:5000/')
@@ -49,3 +17,71 @@ async function fetchTrending() {
 }
 
 fetchTrending();
+
+// NAV FUNCTIONALITY
+// Search Icon Click
+const searchIcon = document.querySelector('.js-search-icon');
+const searchBar = document.querySelector('.js-search');
+let condition = false;
+
+// searchIcon.addEventListener('click', () => {
+//   if (condition === false) {
+//     document.querySelector('.js-nav-center').innerHTML = `
+//       <input type="text" placeholder="Search for a movie..." name="search" id="search" autocomplete="off" class="w-max h-[2.5rem] rounded-full text-white text-center border border-white px-2 py-1 transition duration-300 ease-in-out">`
+//   }
+//   else {
+//     document.querySelector('.js-nav-center').innerHTML = `
+//     <ul class="flex items-center justify-center gap-[2rem] w-max h-full text-[1rem] font-semibold">
+//       <li class="flex items-center justify-center h-full w-max">
+//         <a href="#" class="relative pb-1 after:content-[''] after:absolute after:left-0 after:bottom-0
+//         after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:transition after:duration-300
+//         hover:after:scale-x-100 hover:after:[box-shadow:0_0_12px_#ffffff,0_0_24px_#ffffff]">Home</a>
+//       </li>
+//       <li class="flex items-center justify-center h-full">
+//         <a href="#" class="relative pb-1 after:content-[''] after:absolute after:left-0 after:bottom-0
+//         after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:transition after:duration-300
+//         hover:after:scale-x-100 hover:after:[box-shadow:0_0_12px_#ffffff,0_0_24px_#ffffff]">Movies</a>
+//       </li>
+//       <li class="flex items-center justify-center h-full">
+//         <a href="#" class="relative pb-1 after:content-[''] after:absolute after:left-0 after:bottom-0
+//         after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:transition after:duration-300
+//         hover:after:scale-x-100 hover:after:[box-shadow:0_0_12px_#ffffff,0_0_24px_#ffffff]">Series</a>
+//       </li>
+//       <li class="flex items-center justify-center h-full">
+//         <a href="#" class="relative pb-1 after:content-[''] after:absolute after:left-0 after:bottom-0
+//         after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:transition after:duration-300
+//         hover:after:scale-x-100 hover:after:[box-shadow:0_0_12px_#ffffff,0_0_24px_#ffffff]">Collections</a>
+//       </li>
+//       <li class="flex items-center justify-center h-full">
+//         <a href="#" class="relative pb-1 after:content-[''] after:absolute after:left-0 after:bottom-0
+//         after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:transition after:duration-300
+//         hover:after:scale-x-100 hover:after:[box-shadow:0_0_12px_#ffffff,0_0_24px_#ffffff]">FAQ</a>
+//       </li>
+//     </ul>`
+//   };
+
+//   condition = !condition;
+//   console.log(condition);
+// });
+
+searchIcon.addEventListener('click', () => {
+  if (condition === false) {
+    searchBar.innerHTML = `
+      <input type="text" placeholder="Search for a movie..." name="search" id="search" autocomplete="off" class="w-max h-[2.5rem] rounded-full text-white text-center border border-white px-2 py-1 transition duration-100 ease-in-out focus:outline-none text-sm">`
+  }
+  else {
+    searchBar.innerHTML = ``;
+  }
+
+  // click outside to close search bar
+  document.addEventListener('click', (event) => {
+    if (!searchIcon.contains(event.target) && !searchBar.contains(event.target)) {
+      condition = true;
+      searchBar.innerHTML = '';
+    }
+});
+   
+
+  condition = !condition;
+  console.log(condition);
+})
