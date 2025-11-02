8a5ebebd40f3d5fae8930421861975144ce7df67
indra saputro <indrasaputro65@gmail.com>
2025-11-02 16:19:59 +0700

    prod: init build in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
@@ -7,5 +10,18 @@ export default defineConfig({
   server: {
     host: true, // atau '0.0.0.0'
      allowedHosts: ['.ngrok-free.app']
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        movies: resolve(__dirname, 'movies.html'),
+        series: resolve(__dirname, 'series.html'),
+        faq: resolve(__dirname, 'faq.html'),
+        detailed: resolve(__dirname, 'detailed.html'),
+        collections: resolve(__dirname, 'collections.html'),
+        
+      }
+    }
   }
 })
